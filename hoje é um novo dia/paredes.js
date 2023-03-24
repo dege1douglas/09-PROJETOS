53699636b4a1f201f465b8fec38d481cbe465801
Douglas Gomes <117647275+dege1douglas@users.noreply.github.com>
2023-03-24 11:31:58 -0300

    Update paredes.js

--- hoje é um novo dia/paredes.js
+++ hoje é um novo dia/paredes.js
@@ -1,12 +1,10 @@
 function sacanage(a, b, c){
     console.log(a*b+c)
 }
-sacanage(4,6,3)
+    sacanage(4,6,3)
 
 /*
 equação do segundo grau
-*/
-
 function raiz(){
     var a = parseFloat(document.getElementById("num1").value);
     var b = parseFloat(document.getElementById("num2").value);
@@ -36,7 +34,22 @@ function raiz(){
      }
     }
   }
+*/
 
 /*
-testezando
+testezando 
 */
+function mOver(obj)
+        {
+            obj.innerHTML="SEGREDO DESCOBERTO!"
+        }    
+        function mOut(obj)
+        {
+            obj.innerHTML="Terceiro romance de George Orwell, publicado em 1936, A flor da Inglaterra é uma fábula realista sobre a sociedade de classes inglesa e seu submundo literário, pintada em cores fortes pelo célebre espírito crítico e satírico do autor."
+        }
+        function mOver2(obj){
+            obj.innerHTML='OUTRO SEGREDO!'
+        }
+        function mOut2(obj){
+            obj.innerHTML='Sacana, pare de chacolhar o mouse sobre a tela! hehehehehe'
+        }
